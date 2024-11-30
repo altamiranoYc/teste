ff0844bb4edc9f782350ae1d6ecef95a707ae559
altamiranoYc <ysmn.alt12@gmail.com>
2024-11-30 10:47:01 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,8 @@
+function alternarMenu(){
+    var m = document.getElementById("links");
+    if(m.style.display=="block"){
+        m.style.display="none";
+    } else {
+        m.style.display="block";
+    }  
+}
